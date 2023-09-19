a5a780f69317a3bb4e29fee5e44b4bb36312e483
rabellan <roland.abellano@gmail.com>
2023-09-19 01:40:34 -0700

    added more markdowns

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,3 +12,14 @@
 # The total number of votes each candidate won
 
 # The winner of the election based on popular vote
+
+# Election Results
+# -------------------------
+# Total Votes: 369711
+# -------------------------
+# Charles Casper Stockham: 23.049% (85213)
+# Diana DeGette: 73.812% (272892)
+# Raymon Anthony Doane: 3.139% (11606)
+# -------------------------
+# Winner: Diana DeGette
+# -------------------------
