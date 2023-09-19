7000324dcf122a12082f18c0c3408ae95dd54e43
rabellan <roland.abellano@gmail.com>
2023-09-19 01:38:36 -0700

    Added markdown on README and both main.py

---
+++ PyPoll/main.py
@@ -0,0 +1,14 @@
+# PyPoll Instructions
+# In this Challenge, you are tasked with helping a small, rural town modernize its vote-counting process.
+
+# You will be given a set of poll data called election_data.csv. The dataset is composed of three columns: "Voter ID", "County", and "Candidate". Your task is to create a Python script that analyzes the votes and calculates each of the following values:
+
+# The total number of votes cast
+
+# A complete list of candidates who received votes
+
+# The percentage of votes each candidate won
+
+# The total number of votes each candidate won
+
+# The winner of the election based on popular vote
