3bf8ec4e16ce7458161880794168f2843f83bb42
rabellan <roland.abellano@gmail.com>
2023-09-20 16:19:57 -0700

    added lines for output

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,6 +10,9 @@
 import csv
 from pathlib import Path
 
+# Set PyPoll's variable sets
+total_voters = []
+
 csv_path = Path("Resources/election_data.csv")
 
 with open(csv_path) as csvfile:
@@ -25,10 +28,18 @@ with open(csv_path) as csvfile:
     # all_lists = [row for row in csvreader]
     for row in csvreader:
         print(row[2], row[1], row[0])
-    # print(all_lists)
+        total_voters.append(row[0])
 
+#count number of total votes and assign it to variable   
+total_voter_count = len(total_voters)
 
-    # -----
+# -->>  Print analysis to standard out
+print("\n")
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes:  {total_voter_count}")
+print("-------------------------")
+
 
 #output file --> ../analysis/poll_analysis_data.txt
 analysis_file = Path("analysis/poll_analysis_data.txt")
@@ -36,3 +47,6 @@ analysis_file = Path("analysis/poll_analysis_data.txt")
 with open(analysis_file, "w") as outfile:
 
     outfile.write("Election Results\n")
+    outfile.write("-------------------------\n")
+    outfile.write(f"Total Votes:  {total_voter_count}\n")
+    outfile.write("-------------------------\n")
